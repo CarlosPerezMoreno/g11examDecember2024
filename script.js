9904f0fcd0db54d338a8f10f316c99fb02f1a068
Carlos Pérez <64006791+CarlosPerezMoreno@users.noreply.github.com>
2024-12-16 13:21:39 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+//1- Select all buttons
+
+
+//2- Attach the changeColor function to each button
+colorButtons.forEach(function(button) {
+    
+  });
+  
+//3- Add a click event listener to each button
+function changeColor(event) {
+  // Get the color from the data-color attribute
+
+  // Change the background color of the body
+
+}
+
